e2fb5ab06ba10fe55bd6b33afeb3190813cff1f9
Arun2880 <160330026+Arun2880@users.noreply.github.com>
2025-02-12 10:30:14 +0530

    Delete frontend directory

--- frontend/src/components/One_on_one.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const OneAppointment = () => {
-    return (
-        <div>One_on_one</div>
-    )
-}
-
-export default OneAppointment
--- frontend/src/components/config.jsx
+++
@@ -1,3 +0,0 @@
-const BASE_URL = 'https://localhost:5000';
-
-export default BASE_URL;
